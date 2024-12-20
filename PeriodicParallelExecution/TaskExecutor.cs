6054bf4dad83ce1431b11747845ed6b70cef6a49
madmir <1119735+madmir@users.noreply.github.com>
2024-12-20 23:05:08 +0100

    Add project files.

---
+++ PeriodicParallelExecution/TaskExecutor.cs
@@ -0,0 +1,94 @@
+﻿namespace PeriodicParallelExecution;
+
+public static class TaskExecutor
+{
+    public static async Task PeriodicParallelExecution(
+        this Func<CancellationToken, Task> action, // task to be executed
+        TimeSpan interval,                         // time interval after which new parallel task for the action is added
+        int maxTaskInstances,                      // maximum parallel instances, after reached some need to complete before new one is added
+        CancellationToken cancellationToken, 
+        string name)
+    {
+        var intervalTask = Task.Delay(interval, cancellationToken); // this task is used to be notified when an interval expires
+        var actionTask = action(cancellationToken);
+
+        List<Task> tasks = new List<Task> { intervalTask, actionTask };
+        Console.WriteLine("\n + Time: \t" + DateTime.Now.ToString() + ". Concurrent task count: \t" + (tasks.Count - 1) + ". Id: \t" + actionTask.Id + ". Added new parallel task.");
+
+        while (true)
+        {
+            var completedTask = await Task.WhenAny(tasks);
+
+            if (completedTask.IsCanceled) // triggered by cts.Cancel();
+            {
+                Console.WriteLine("\n * Time: \t" + DateTime.Now.ToString() + ". Concurrent task count: \t" + (tasks.Count - 1) + ". Cancellation requested.");
+                try
+                {
+                    await Task.WhenAll(tasks.ToArray());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" * Time: \t" + DateTime.Now.ToString() + ". Concurrent task count: \t" + (tasks.Count - 1) + ". Caught exception: " + ex.Message);
+                    tasks.Remove(intervalTask);
+                    var count = 0;
+                    foreach (var task in tasks)
+                    {
+                        if (task.IsCanceled)
+                        {
+                            count++;
+                            Console.WriteLine(" - Time: \t" + DateTime.Now.ToString() + ". Concurrent task count: \t" + ((tasks.Count) - count) + ". Id: \t" + task.Id + ". Task cancelled.");
+                        }
+                    }
+
+                    if (count == tasks.Count)
+                    {
+                        Console.WriteLine(" * All tasks cancelled.");
+                    }
+                    else
+                    {
+                        // ending up here means not all children tasks were cancelled. Happens when there is no ct.ThrowIfCancellationRequested() call in the action method.
+                        Console.WriteLine(" * Some tasks are not cancelled. Make sure action method handles cancellation token properly.");
+                    }
+                }
+                finally
+                {
+                    Console.WriteLine(" * Exiting loop.");
+                }
+                break;
+            }
+
+            if (completedTask == intervalTask) // an interval expired, now needs to be renewed
+            {
+
+                intervalTask = Task.Delay(interval, cancellationToken);
+                tasks.Remove(completedTask);
+                tasks.Add(intervalTask);
+                Console.WriteLine("\n * Time: \t" + DateTime.Now.ToString() + ". Concurrent task count: \t" + (tasks.Count - 1) + ". Interval cycle completed. Duration: " + interval.Hours + ":" + interval.Minutes + ":" + interval.Seconds);
+                if (tasks.Count - 1 < maxTaskInstances)
+                {
+                    actionTask = action(cancellationToken);
+                    tasks.Add(actionTask);
+                    Console.WriteLine(" + Time: \t" + DateTime.Now.ToString() + ". Concurrent task count: \t" + (tasks.Count - 1) + ". Id: \t" + actionTask.Id + ". Added new parallel task.");
+                }
+                else
+                {
+                    Console.WriteLine(" * Not adding new tasks, queue full (" + (tasks.Count - 1) + ").");
+                }
+            }
+            else // one of the action tasks completed - either with exception if faulted or successfully
+            {
+                tasks.Remove(completedTask);
+                if (completedTask.IsFaulted)
+                {
+                    Console.WriteLine("\n - Time: \t" + DateTime.Now.ToString() + ". Concurrent task count: \t" + (tasks.Count - 1) + ". Id: \t" + completedTask.Id + ". Faulted task. Caught exception: " + completedTask.Exception?.Message);
+                }
+                else // completedTask.IsCompletedSuccessfully
+                {
+                    Console.WriteLine("\n - Time: \t" + DateTime.Now.ToString() + ". Concurrent task count: \t" + (tasks.Count - 1) + ". Id: \t" + completedTask.Id + ". Task completed successfully.");
+                }
+            }
+        }
+
+        Console.WriteLine(" * \"" + name + "\" work is finished.");
+    }
+}
---
+++ SampleConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PeriodicParallelExecution;
+
+Console.WriteLine("Starting...");
+Console.WriteLine("Press ENTER to exit.");
+
+var interval = TimeSpan.FromSeconds(7);
+CancellationTokenSource? cts = new CancellationTokenSource();
+
+try
+{
+    var t = Task.Run(() => TaskExecutor.PeriodicParallelExecution(ActionMethod, interval, 4, cts.Token, "Sample Task"));
+    //var t = TaskExecutor.PeriodicParallelExecution(ActionMethod, interval, 4, cts.Token, "Sample Task");
+
+    Console.ReadLine();
+    cts.Cancel();
+}
+catch
+{
+
+}
+finally
+{
+    cts.Dispose();
+    cts = null;
+}
+
+Console.ReadLine();
+
+static async Task<int> ActionMethod(CancellationToken ct)
+{
+    // faking action that lasts one minute
+    for (int i = 0; i < 60; i++)
+    {
+        await Task.Delay(1000);
+        ct.ThrowIfCancellationRequested();
+    }
+    return 0;
+}
+
+static async Task<int> ActionMethodException(CancellationToken ct)
+{
+    // same as ActionMethod but throws exception after 15 seconds. Used to test how exception is handled/propagated.
+    for (int i = 0; i < 60; i++)
+    {
+        await Task.Delay(1000);
+        if (i == 15) throw new Exception("Fake exception");
+        ct.ThrowIfCancellationRequested();
+    }
+    return 0;
+}
